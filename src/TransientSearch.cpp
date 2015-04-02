f961ffd5877055e8a617b4aeeb52574f4f436b4c
Alessio Sclocco <alessio@sclocco.eu>
2015-04-02 16:42:05 +0200

    dedispersedData is read/write.

--- src/TransientSearch.cpp
+++ src/TransientSearch.cpp
@@ -200,7 +200,7 @@ int main(int argc, char * argv[]) {
     shifts_d = cl::Buffer(*clContext, CL_MEM_READ_ONLY, shifts->size() * sizeof(float), 0, 0);
     for ( unsigned int beam = 0; beam < obs.getNrBeams(); beam++ ) {
       dispersedData_d[beam] = cl::Buffer(*clContext, CL_MEM_READ_ONLY, dispersedData[beam].size() * sizeof(dataType), 0, 0);
-      dedispersedData_d[beam] = cl::Buffer(*clContext, CL_MEM_WRITE_ONLY, obs.getNrDMs() * obs.getNrSamplesPerPaddedSecond() * sizeof(dataType), 0, 0);
+      dedispersedData_d[beam] = cl::Buffer(*clContext, CL_MEM_READ_WRITE, obs.getNrDMs() * obs.getNrSamplesPerPaddedSecond() * sizeof(dataType), 0, 0);
       snrData_d[beam] = cl::Buffer(*clContext, CL_MEM_WRITE_ONLY, snrData[beam].size() * sizeof(float), 0, 0);
     }
     clQueues->at(clDeviceID)[0].enqueueWriteBuffer(shifts_d, CL_TRUE, 0, shifts->size() * sizeof(float), reinterpret_cast< void * >(shifts->data()));
