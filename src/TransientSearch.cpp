c23e1a6443135a2bdf9aba251bd428dea4867c1c
Alessio Sclocco <alessio@sclocco.eu>
2015-11-03 10:27:56 +0100

    TODO.

--- src/TransientSearch.cpp
+++ src/TransientSearch.cpp
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// TODO: Merge of candidates left as an option, not the default
 // TODO: PSRDada multibeam
 
 #include <iostream>
