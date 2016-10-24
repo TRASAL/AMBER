3b5ec8428f8f0a3075a58fd1e10c23cd2a242cde
Alessio Sclocco <alessio@sclocco.eu>
2016-10-24 16:02:18 +0200

    Typo.

--- include/BeamDriver.hpp
+++ include/BeamDriver.hpp
@@ -1,4 +1,4 @@
-s// Copyright 2016 Alessio Sclocco <a.sclocco@vu.nl>
+// Copyright 2016 Alessio Sclocco <a.sclocco@vu.nl>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
