12ed17177b918dde1c07ca8cb3af125f99bcc84f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-05-22 11:33:18 +0200

    Fixed a bug in step two of dedispersion. Using the wrong number of work-items in dim2.

--- src/TransientSearch.cpp
+++ src/TransientSearch.cpp
@@ -510,7 +510,7 @@ int main(int argc, char * argv[]) {
         std::cout << dedispersionStepOneParameters.at(deviceName)->at(obs.getNrDMsSubbanding())->print() << std::endl;
         std::cout << std::endl;
     }
-    dedispersionStepTwoGlobal = cl::NDRange(isa::utils::pad(obs.getNrSamplesPerBatchSubbanding() / dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrItemsD0(), dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrThreadsD0()), obs.getNrDMs() / dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrItemsD1(), obs.getNrBeams() * obs.getNrDMsSubbanding());
+    dedispersionStepTwoGlobal = cl::NDRange(isa::utils::pad(obs.getNrSamplesPerBatchSubbanding() / dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrItemsD0(), dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrThreadsD0()), obs.getNrDMs() / dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrItemsD1(), obs.getNrSynthesizedBeams() * obs.getNrDMsSubbanding());
     dedispersionStepTwoLocal = cl::NDRange(dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrThreadsD0(), dedispersionStepTwoParameters.at(deviceName)->at(obs.getNrDMs())->getNrThreadsD1(), 1);
     if ( DEBUG ) {
       std::cout << "DedispersionStepTwo" << std::endl;
