6a6468178cd76a20c5b6ea06f5756e170cce16ae
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-06-28 16:30:35 +0200

    Changed output description.

--- src/TransientSearch.cpp
+++ src/TransientSearch.cpp
@@ -616,9 +616,9 @@ int main(int argc, char * argv[]) {
   searchTimer.start();
   output.open(outputFile + ".trigger");
   if ( compactResults ) {
-    output << "# beam batch sample integration time DM compactedDMs SNR" << std::endl;
+    output << "# beam batch sample integration_step time DM compacted_DMs SNR" << std::endl;
   } else {
-    output << "# beam batch sample integration time DM SNR" << std::endl;
+    output << "# beam batch sample integration_step time DM SNR" << std::endl;
   }
   for ( unsigned int batch = 0; batch < obs.getNrBatches(); batch++ ) {
     triggeredEvents_t triggeredEvents(obs.getNrSynthesizedBeams());
