cd14dd2c2abf21b84db3440c977d4e5cd61ddd0f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-01-29 13:05:25 +0100

    Removed unnecessary includes.

--- include/CommandLine.hpp
+++ include/CommandLine.hpp
@@ -13,10 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string>
-#include <iostream>
-#include <exception>
-
 #include "configuration.hpp"
 
 #pragma once
