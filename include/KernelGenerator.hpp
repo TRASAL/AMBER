4a01624534092eef43a8a77ff4575a9bc649b4d5
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-01-29 13:05:52 +0100

    Reorganized includes.

--- include/KernelGenerator.hpp
+++ include/KernelGenerator.hpp
@@ -13,10 +13,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <vector>
-#include <string>
-
 #include "configuration.hpp"
+
+#include "CommandLine.hpp"
 
 #pragma once
 
