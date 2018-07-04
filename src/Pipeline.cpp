baaf1e15bc9a2ed9224869a7ce6a9356e011a009
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-07-04 16:36:54 +0200

    Print the batch in dumped files.

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -44,7 +44,6 @@ void pipeline(const OpenCLRunTime &openclRunTime, const AstroData::Observation &
             hostMemoryDumpFiles.medianOfMediansAbsoluteDeviationData.open("medianOfMediansAbsoluteDeviation.dump");
         }
     }
-
     timers.search.start();
     outputTrigger.open(dataOptions.outputFile + ".trigger");
     if (!outputTrigger)
@@ -88,6 +87,27 @@ void pipeline(const OpenCLRunTime &openclRunTime, const AstroData::Observation &
             continue;
         }
 
+        if (options.dataDump)
+        {
+            if (options.subbandDedispersion)
+            {
+                hostMemoryDumpFiles.subbandedData << "Batch: " << batch << std::endl;
+            }
+            hostMemoryDumpFiles.dedispersedData << "Batch: " << batch << std::endl;
+            hostMemoryDumpFiles.integratedData << "Batch: " << batch << std::endl;
+            if (options.snrMode == SNRMode::Standard)
+            {
+                hostMemoryDumpFiles << "Batch: " << batch << std::endl;
+                hostMemoryDumpFiles.snrSamplesData << "Batch: " << batch << std::endl;
+            }
+            else if (options.snrMode == SNRMode::Momad)
+            {
+                hostMemoryDumpFiles.maxValuesData << "Batch: " << batch << std::endl;
+                hostMemoryDumpFiles.maxIndicesData << "Batch: " << batch << std::endl;
+                hostMemoryDumpFiles.mediansOfMediansData << "Batch: " << batch << std::endl;
+                hostMemoryDumpFiles.medianOfMediansAbsoluteDeviationData << "Batch: " << batch << std::endl;
+            }
+        }
         // Dedispersion
         if (options.subbandDedispersion)
         {
@@ -470,7 +490,7 @@ void pipeline(const OpenCLRunTime &openclRunTime, const AstroData::Observation &
                             hostMemoryDumpFiles.medianOfMediansAbsoluteDeviationData << hostMemory.medianOfMediansAbsoluteDeviation.at((sBeam * observation.getNrDMs(false, deviceOptions.padding.at(deviceOptions.deviceName) / sizeof(float))) + dm);
                             hostMemoryDumpFiles.medianOfMediansAbsoluteDeviationData << " ";
                         }
-                        hostMemoryDumpFiles.maxValuesData << std::endl;
+                        F << std::endl;
                         hostMemoryDumpFiles.maxIndicesData << std::endl;
                         hostMemoryDumpFiles.mediansOfMediansData << std::endl;
                         hostMemoryDumpFiles.medianOfMediansAbsoluteDeviationData << std::endl;
