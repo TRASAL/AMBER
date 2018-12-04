cd4fcc96cc55eee000f5fc430642f3d77e197433
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-12-04 14:04:39 +0100

    Error in printing results. Fixed.

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -1386,13 +1386,13 @@ int printResults(const unsigned int batch, const AstroData::Observation &observa
                     if (dataOptions.dataPSRDADA || dataOptions.streamingMode)
                     {
                         outputTrigger << event.beam << " " << (batch - delay) << " " << event.sample << " " << integration;
-                        outputTrigger << " " << (((batch - delay) * (observation.getNrSamplesPerBatch() / observation.getDownsampling())) + (event.sample * observation.getDownsampling() * integration)) * observation.getSamplingTime() << " ";
+                        outputTrigger << " " << (((batch - delay) * observation.getNrSamplesPerBatch()) + (event.sample * observation.getDownsampling() * integration)) * observation.getSamplingTime() << " ";
                         outputTrigger << event.DM << " " << firstDM + (event.DM * observation.getDMStep()) << " " << event.SNR << std::endl;
                     }
                     else
                     {
                         outputTrigger << event.beam << " " << batch << " " << event.sample << " " << integration << " ";
-                        outputTrigger << ((batch * (observation.getNrSamplesPerBatch() / observation.getDownsampling())) + (event.sample * observation.getDownsampling() * integration)) * observation.getSamplingTime() << " ";
+                        outputTrigger << ((batch * observation.getNrSamplesPerBatch()) + (event.sample * observation.getDownsampling() * integration)) * observation.getSamplingTime() << " ";
                         outputTrigger << event.DM << " " << firstDM + (event.DM * observation.getDMStep()) << " " << event.SNR << std::endl;
                     }
                 }
