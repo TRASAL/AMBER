8a1337e917e0dcffbe42308b80c6182650e24e2a
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-06-14 12:24:41 +0200

    Sleep for 3 seconds between attempts.

--- src/AMBER.cpp
+++ src/AMBER.cpp
@@ -242,7 +242,7 @@ int main(int argc, char *argv[])
                 return 1;
             }
         }
-        std::this_thread::sleep_for(3s);
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     } 
 
     // Memory allocation
